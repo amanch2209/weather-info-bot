a2a48beb82b73dac22878c37233755dcb1baa4b6
Aman Chauhan <amanch2209@gmail.com>
2024-01-05 11:25:27 +0530

    Bot Instantiated

---
+++ telegram-bot/src/telegram-bot.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@nestjs/common';
+import * as telegramBot from 'node-telegram-bot-api';
+import axios from 'axios';
+import { config } from 'dotenv';
+config();
+
+const TELEGRAM_BOT_TOKEN = process.env.TELEGRAM_TOKEN;
+const OPEN_WEATHER_API = process.env.OPEN_WEATHER;
+
+@Injectable()
+export class BotService{
+    constructor(){
+        const bot = new telegramBot(TELEGRAM_BOT_TOKEN , {polling:true});
+        bot.onText(/\/start/, async(msg)=>{
+            const ID = msg.chat.id;
+            const name = msg.from.first_name;
+            const input = msg.text;
+            bot.sendMessage(ID,`Hi ${name}, Welcome to the Weather Info Bot`);
+            bot.sendMessage(ID, `Please type the City Name for weather updates`);
+            try{
+                const response = await axios.get(
+                    `https://api.openweathermap.org/data/2.5/weather?q=${input}&appid=${OPEN_WEATHER_API}`,
+                )
+                const data = response.data;
+                const weather = data.weather[0].describe;
+                const message = `${weather}`;
+                bot.sendMessage(ID,message);
+            }catch(error){
+                bot.sendMessage(ID, `${error}`);
+            }
+        })
+    }
+}
